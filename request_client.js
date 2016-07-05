59c367859b799ed79c1787490726b6666c0114a1
maxahn <ahnmaximus@gmail.com>
2016-07-05 15:09:15 -0700

    scraped files will now be added to csv file

--- request_client.js
+++ request_client.js
@@ -1,15 +1,20 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var csvWriter = require('csv-write-stream');
+const fs = require('fs');
+
 const TESTURL= 'http://substack.net/images/';
 var url = 'http://substack.net';
 
+var writer = csvWriter({headers: ['FilePermission', 'AbsoluteURL', 'FileType']});
+writer.pipe(fs.createWriteStream('scraped_files.csv'));
 
 request(TESTURL, function(error, response, body) {   //required: file permission, absolute url, file type
   if (!error && response.statusCode == 200) {
     $ = cheerio.load(body);
     var files = [];
     $('tr').each(function(i, elem) {
-      // console.log($(elem).find('code').first().text() + ' ' + $(elem).find('a').text() + ' ' + $(elem).find('a').attr('href'));
+
       var fileExt = $(elem).find('a').text().split('.')[1];
       var filePerm = $(elem).find('code').first().text(); 
       var absUrl =  url + $(elem).find('a').attr('href');
@@ -21,8 +26,18 @@ request(TESTURL, function(error, response, body) {   //required: file permission
           filetype: fileExt 
         }
       }
-      console.log(files[i]);
+
+      for (var counter = 0; counter < files.length; counter++) {
+        if (files[counter]) {
+        // console.log(files[counter].filepermission, files[counter].absoluteurl, files[counter].filetype);
+        writer.write(
+          [files[counter].filepermission, files[counter].absoluteurl, files[counter].filetype]
+        ); 
+        }
+      }
+//        writer.end();
     });
+
   }
 });
 
